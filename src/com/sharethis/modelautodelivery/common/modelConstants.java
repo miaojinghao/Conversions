aa8419570174466c526e0af2590da29bd16290ac
Jinghao Miao <jmiao@sharethis.com>
2014-10-16 15:58:32 -0700

    remove all files

--- src/com/sharethis/modelautodelivery/common/modelConstants.java
+++
@@ -1,6 +0,0 @@
-package com.sharethis.modelautodelivery.common;
-
-public class modelConstants {
-	public static final String LOGGER_NAME = "model_auto_threshold";
-	public static final int NUM_REDUCER = 100;
-}
